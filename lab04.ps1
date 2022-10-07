ea97bde4a9e2c28ea201c39f596db1094734d957
shannon <shannon@ubuntudev>
2022-10-07 06:51:08 +0000

    first commit

---
+++ lab04.ps1
@@ -0,0 +1,11 @@
+# Script Name: GPO Password Length Requirement
+# Author: Shay Crane
+# Date of last revision: 10/03/2022
+# Description of purpose: Enables Windows Server GPO 
+# user password Length Requirement that forces user 
+# to create a password of 14 characters or more
+
+
+
+
+net accounts /minpwlen:14
